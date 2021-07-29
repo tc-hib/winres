1bf8686351e2cd8d313dd47a303b5421f9234856
Thomas Combeléran <55949036+tc-hib@users.noreply.github.com>
2021-07-29 12:54:11 +0200

    Fix doc indentation

--- doc.go
+++ doc.go
@@ -1,110 +1,111 @@
 /*
-	Package winres provides functions to create a resource section for Windows executables.
-	This is where the application's icon, manifest, and version information are stored.
 
-	Basic usage
+Package winres provides functions to create a resource section for Windows executables.
+This is where the application's icon, manifest, and version information are stored.
 
-	Create an empty ResourceSet, call the Set method to add resources to it
-	and then use the WriteObject method to produce an object file.
+Basic usage
 
-	Each resource must be named, so it can later be accessed with FindResource, LoadImage, etc.
-	To name a resource, you may use an int that you cast to winres.ID, or a string that you cast to winres.Name.
+Create an empty ResourceSet, call the Set method to add resources to it
+and then use the WriteObject method to produce an object file.
 
-	 rs := winres.ResourceSet{}
-	 rs.Set(winres.RT_RCDATA, winres.Name("MYDATA"), 0, 0, []byte("some data"))
-	 rs.WriteObject(someFile, winres.ArchAMD64)
-	 rs.WriteObject(anotherFile, winres.ArchI386)
+Each resource must be named, so it can later be accessed with FindResource, LoadImage, etc.
+To name a resource, you may use an int that you cast to winres.ID, or a string that you cast to winres.Name.
 
-	Embedding resources in a Go built application
+ rs := winres.ResourceSet{}
+ rs.Set(winres.RT_RCDATA, winres.Name("MYDATA"), 0, 0, []byte("some data"))
+ rs.WriteObject(someFile, winres.ArchAMD64)
+ rs.WriteObject(anotherFile, winres.ArchI386)
 
-	winres produces a linkable COFF object.
-	Save it in your project's root directory with extension ``syso''
-	and it will be automatically included by ``go build''.
+Embedding resources in a Go built application
 
-	It is recommended to name this object with target suffixes,
-	so that the ``go build'' command automatically links the proper object for each target.
+winres produces a linkable COFF object.
+Save it in your project's root directory with extension ``syso''
+and it will be automatically included by ``go build''.
 
-	For example:
-	 rsrc_windows_amd64.syso
-	 rsrc_windows_386.syso
+It is recommended to name this object with target suffixes,
+so that the ``go build'' command automatically links the proper object for each target.
 
-	Example
+For example:
+ rsrc_windows_amd64.syso
+ rsrc_windows_386.syso
 
-	Embedding an icon, version information, and a manifest:
+Example
 
-	 import (
-	 	"image"
-	 	"log"
-	 	"os"
+Embedding an icon, version information, and a manifest:
 
-	 	"github.com/tc-hib/winres"
-	 	"github.com/tc-hib/winres/version"
-	 )
+ import (
+	"image"
+	"log"
+	"os"
 
-	 func main() {
-	 	// First create an empty resource set
-	 	rs := winres.ResourceSet{}
+	"github.com/tc-hib/winres"
+	"github.com/tc-hib/winres/version"
+ )
 
-	 	// Make an icon group from a png file
-	 	f, err := os.Open("icon.png")
-	 	if err != nil {
-	 		log.Fatalln(err)
-	 	}
-	 	defer f.Close()
-	 	img, _, err := image.Decode(f)
-	 	if err != nil {
-	 		log.Fatalln(err)
-	 	}
-	 	f.Close()
-	 	icon, _ := winres.NewIconFromResizedImage(img, nil)
+ func main() {
+	// First create an empty resource set
+	rs := winres.ResourceSet{}
 
-	 	// Add the icon to the resource set, as "APPICON"
-	 	rs.SetIcon(winres.Name("APPICON"), icon)
+	// Make an icon group from a png file
+	f, err := os.Open("icon.png")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+	img, _, err := image.Decode(f)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	f.Close()
+	icon, _ := winres.NewIconFromResizedImage(img, nil)
 
-	 	// Make a VersionInfo structure
-	 	vi := version.Info{
-	 		FileVersion:    [4]uint16{1, 0, 0, 0},
-	 		ProductVersion: [4]uint16{1, 0, 0, 1},
-	 	}
-	 	vi.Set(0, version.ProductName, "A Windows Application")
-	 	vi.Set(0, version.ProductVersion, "v1.0.0.1")
+	// Add the icon to the resource set, as "APPICON"
+	rs.SetIcon(winres.Name("APPICON"), icon)
 
-	 	// Add the VersionInfo to the resource set
-	 	rs.SetVersionInfo(vi)
+	// Make a VersionInfo structure
+	vi := version.Info{
+		FileVersion:    [4]uint16{1, 0, 0, 0},
+		ProductVersion: [4]uint16{1, 0, 0, 1},
+	}
+	vi.Set(0, version.ProductName, "A Windows Application")
+	vi.Set(0, version.ProductVersion, "v1.0.0.1")
 
-	 	// Add a manifest
-	 	rs.SetManifest(winres.AppManifest{
-	 		ExecutionLevel:      RequireAdministrator,
-	 		DPIAwareness:        DPIPerMonitorV2,
-	 		UseCommonControlsV6: true,
-	 	})
+	// Add the VersionInfo to the resource set
+	rs.SetVersionInfo(vi)
 
-	 	// Create an object file for amd64
-	 	out, err := os.Create("rsrc_windows_amd64.syso")
-	 	defer out.Close()
-	 	if err != nil {
-	 		log.Fatalln(err)
-	 	}
-	 	err = rs.WriteObject(out, winres.ArchAMD64)
-	 	if err != nil {
-	 		log.Fatalln(err)
-	 	}
-	 }
+	// Add a manifest
+	rs.SetManifest(winres.AppManifest{
+		ExecutionLevel:      RequireAdministrator,
+		DPIAwareness:        DPIPerMonitorV2,
+		UseCommonControlsV6: true,
+	})
 
-	Localization
+	// Create an object file for amd64
+	out, err := os.Create("rsrc_windows_amd64.syso")
+	defer out.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = rs.WriteObject(out, winres.ArchAMD64)
+	if err != nil {
+		log.Fatalln(err)
+	}
+ }
 
-	You can provide different resources depending on the user's langage.
+Localization
 
-	To do so, you should provide a language code identifier (LCID) to the Set method.
+You can provide different resources depending on the user's langage.
 
-	A list of LCIDs is available there: https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-lcid/
+To do so, you should provide a language code identifier (LCID) to the Set method.
 
-	As an example, the LCID for en-US is 0x0409.
+A list of LCIDs is available there: https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-lcid/
 
-	Other functions
+As an example, the LCID for en-US is 0x0409.
 
-	winres can do a few more things: extract resources from an executable, replace resources in an executable,
-	export cursors or icons...
+Other functions
+
+winres can do a few more things: extract resources from an executable, replace resources in an executable,
+export cursors or icons...
 
 */
 package winres
